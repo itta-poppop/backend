bda732bacdf1fae188b104f606e7d7e0c6bbb0bd
JonghyeokNam <njh9750@naver.com>
2025-05-25 02:33:20 +0900

    Refactor #PP-49: 팝업 레포지토리 최신화

--- src/main/java/com/example/poppop/domain/popup/repository/PopUpRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.poppop.domain.popup.repository;
-
-import com.example.poppop.domain.popup.entity.PopUp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PopUpRepository extends JpaRepository<PopUp, Long> {
-}
--- src/main/java/com/example/poppop/domain/popup/repository/PopupRepository.java
+++ src/main/java/com/example/poppop/domain/popup/repository/PopupRepository.java
@@ -1,7 +1,27 @@
 package com.example.poppop.domain.popup.repository;
 
-import com.example.poppop.domain.popup.entity.PopUp;
+import com.example.poppop.domain.popup.entity.Popup;
+import io.lettuce.core.dynamic.annotation.Param;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface PopUpRepository extends JpaRepository<PopUp, Long> {
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PopupRepository extends JpaRepository<Popup, Long> {
+
+    // startDate가 오늘 이후인 팝업을 오름차순 정렬해서 페이징 조회
+    @Query("SELECT p FROM Popup p WHERE p.startDate BETWEEN :now AND :end order by p.startDate asc")
+    List<Popup> findPlannedPopups(@Param("now") LocalDate now, @Param("end") LocalDate end , Pageable pageable);
+
+    @Query("select p FROM Popup p order by p.viewCount desc")
+    List<Popup> findTrendPopups(Pageable pageable);
+
+    @Query("select p from Popup p where p.title like %:title%")
+    List<Popup> findSearchedPopups(Pageable pageable,String title);
+    /*@Modifying
+    @Transactional
+    @Query("UPDATE Post p SET p.viewCnt = p.viewCnt + 1 WHERE p.postId = :postId")
+    void incrementViewCount(@Param("postId") Long postId);*/
 }
